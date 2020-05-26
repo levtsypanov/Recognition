5bae530302166abfb2f5ee06fa6b0df1e687faa1
levtsypanov <levdark7@gmail.com>
2020-05-26 10:23:39 +0300

    Init 🚀🚀

---
+++ README.md
@@ -0,0 +1,7 @@
+ <div align="center">
+<img width="134" src="https://vk.com/images/apps/mini_apps/vk_mini_apps_logo.svg">
+<p align="center"><b>Recognition</b></p>
+<p align="center">Apps Recognition for VK Mini Apps Platform</p>
+<p align="center">
+  <a href="https://vk.com/id310116789">По всем вопросам</a>
+</p>
---
+++ precache-manifest.01b3a31c458e534b07b71006e1a1ae2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944df9c35c2cdabf7775d8cd9655e659",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f5b97585bb692b0c002",
+    "url": "/static/css/2.a3159a08.chunk.css"
+  },
+  {
+    "revision": "0b42e32d864622c4acd3",
+    "url": "/static/css/main.74110257.chunk.css"
+  },
+  {
+    "revision": "1f5b97585bb692b0c002",
+    "url": "/static/js/2.18b2e350.chunk.js"
+  },
+  {
+    "revision": "0b42e32d864622c4acd3",
+    "url": "/static/js/main.267057fc.chunk.js"
+  },
+  {
+    "revision": "20b6a40eb6a14a01f457",
+    "url": "/static/js/runtime-main.22191d3d.js"
+  },
+  {
+    "revision": "87c938e6d474e6c5f1b2c8592382215b",
+    "url": "/static/media/wiki.87c938e6.svg"
+  }
+]);
